60a1d3f6a0e4423320176af001c245b180244303
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-31 13:50:31 -0600

    update schema to reflect application status

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_03_31_011516) do
     t.string "state"
     t.string "zip_code"
     t.string "description"
+    t.string "status", default: "In Progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
