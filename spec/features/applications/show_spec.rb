0d29712cddf99487d288c2a5a8841230fa644115
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-31 13:56:39 -0600

    update before each to use test_data instead

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,14 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'the applications show page' do
   before :each do
-    @shelter_1 = Shelter.create!(foster_program: true, name: "Chatfield Vet", city: "Littleton", rank: 1)
-    @application_1 = Application.create!(applicant: "Joe Smith", street_address: "123 Everywhere Street", city: "Anytown", state: "NY", zip_code: "13827", description: "I am lonely and need a pet.")
-    @application_2 = Application.create!(applicant: "Jane Doe", street_address: "455 Mulberry Street", city: "Springfield", state: "MA", zip_code: "53245", description: "I like dogs.")
-    @pet_1 = Pet.create!(adoptable: true, age: 2, breed: "Chow Chow", name: "Spot", shelter_id: @shelter_1.id)
-    @pet_2 = Pet.create!(adoptable: true, age: 2, breed: "Labrador", name: "Spike", shelter_id: @shelter_1.id)
-    @joe_spot = PetsApplication.create!(application: @application_1, pet: @pet_1)
-    @joe_spike = PetsApplication.create!(application: @application_1, pet: @pet_2)
-    @jane_spike = PetsApplication.create!(application: @application_2, pet: @pet_2)
+   test_data
   end
   describe 'As a visitor, when I visit an applications show page' do
     it 'exists' do
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -7,16 +7,9 @@ RSpec.describe Application do
   end
 
   describe '#instance_methods' do
-    before :each do
-      @shelter_1 = Shelter.create!(foster_program: true, name: "Chatfield Vet", city: "Littleton", rank: 1)
-      @application_1 = Application.create!(applicant: "Joe Smith", street_address: "123 Everywhere Street", city: "Anytown", state: "NY", zip_code: "13827", description: "I am lonely and need a pet.")
-      @application_2 = Application.create!(applicant: "Jane Doe", street_address: "455 Mulberry Street", city: "Springfield", state: "MA", zip_code: "53245", description: "I like dogs.")
-      @pet_1 = Pet.create!(adoptable: true, age: 2, breed: "Chow Chow", name: "Spot", shelter_id: @shelter_1.id)
-      @pet_2 = Pet.create!(adoptable: true, age: 2, breed: "Labrador", name: "Spike", shelter_id: @shelter_1.id)
-      @joe_spot = PetsApplication.create!(application: @application_1, pet: @pet_1)
-      @joe_spike = PetsApplication.create!(application: @application_1, pet: @pet_2)
-      @jane_spike = PetsApplication.create!(application: @application_2, pet: @pet_2)
-    end
+  before :each do
+    test_data
+   end
     describe '#find_app_pets' do
       it 'returns the pets applied for on an application' do
         expect(@application_1.find_app_pets).to eq([@pet_1, @pet_2])
