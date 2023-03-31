6485b4efa567fa26e1867945978537c8bd771d6c
Katie Lonsdale <katie952@gmail.com>
2023-03-30 20:17:56 -0600

    feat: add find_app_pets method

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,9 @@
 class Application < ApplicationRecord
   has_many :pets_applications
   has_many :pets, through: :pets_applications
+
+  def find_app_pets
+    pets = PetsApplication.select(:pet_id).where(application_id: id)
+    Pet.where(id: pets)
+  end
 end
