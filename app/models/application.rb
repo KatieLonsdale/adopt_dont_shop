b482d6ccab5ef76a95143c034815004cc238f17c
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-31 19:31:18 -0600

    feat: add validations to application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,13 @@ class Application < ApplicationRecord
   has_many :pets_applications
   has_many :pets, through: :pets_applications
 
+  validates :applicant, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :description, presence: true
+  
   def find_app_pets
     pets = PetsApplication.select(:pet_id).where(application_id: id)
     Pet.where(id: pets)
