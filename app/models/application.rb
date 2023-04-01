111afbbe8306d6e955c6454c850ce0d71b624e19
Katie Lonsdale <katie952@gmail.com>
2023-04-01 15:39:09 -0600

    feat: add pets_selected? method

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,6 +7,11 @@ class Application < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
+
+  def pets_selected?
+    pets = find_app_pets
+    pets.empty? ? false : true
+  end
   
   def find_app_pets
     pets = PetsApplication.select(:pet_id).where(application_id: id)
