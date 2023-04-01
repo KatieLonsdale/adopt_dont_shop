214e7e509256a517d8d275f5d5afc0f291551999
Katie Lonsdale <katie952@gmail.com>
2023-04-01 15:49:13 -0600

    test: add test that submit section does not show if no pets are added to app

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -111,40 +111,30 @@ RSpec.describe 'the applications show page' do
   describe 'and one or more pets are added to the application' do
     it 'has a section to enter why I would be a good owner' do
       visit "/applications/#{@application_1.id}"
+      
       within("#submit-app") do
         expect(page).to have_content("Why would you be a good owner for these pet(s)?")
         fill_in(:description, with: "Really long explanation")
+        click_button("Submit Application")
+        expect(current_path).to eq("/applications/#{@application_1.id}")
+      end
+      within("#application-info") do
+        expect(page).to have_content("Name: #{@application_1.applicant}")
+        expect(page).to have_content("Street Address: #{@application_1.street_address}")
+        expect(page).to have_content("City: #{@application_1.city}")
+        expect(page).to have_content("State: #{@application_1.state}")
+        expect(page).to have_content("Zip Code: #{@application_1.zip_code}")
+        expect(page).to have_content("Really long explanation")
+      end
+      within('#application-status') do
+        expect(page).to have_content("Application Status: Pending")
       end
     end
-    xit 'has a section to submit my application' do
-      visit "/applications/#{@application_1.id}"
-      # within("#submit-app") do
-        expect(page).to have_selector(:link_or_button, "Submit my application.")
-        click_button("Submit my application.")
-        expect(current_path).to eq("/applications/#{@application_1.id}")
-      # end
-      # within("#application-status") do
-        expect(page).to have_content("Application Status: Pending")
-      # end
-    end
-    xit 'does not show this section if I have no pets added to my application' do
+    it 'does not show this section if I have no pets added to my application' do
       visit "/applications/#{@application_6.id}"
-      # within("#submit-app") do
-        expect(page).to have_no_content
-      # end
-
+      within("#submit-app") do
+        expect(page).to have_no_content("Why would you be a good owner for these pet(s)?")
+      end
     end
   end
 end
-
-
-# And I have added one or more pets to the application
-# Then I see a section to submit my application
-# And in that section I see an input to enter why I would make a good owner for these pet(s) - is this required?
-# When I fill in that input
-# And I click a button to submit this application
-# Then I am taken back to the application's show page
-# And I see an indicator that the application is "Pending"
-# And I see all the pets that I want to adopt - already covered
-# And I do not see a section to add more pets to this application
-# ^^^already have test for that in previous block
