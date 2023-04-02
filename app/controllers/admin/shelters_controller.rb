bf44f476f442fbc9400804a004bcc28266ab4788
Katie Lonsdale <118778749+KatieLonsdale@users.noreply.github.com>
2023-04-02 12:12:50 -0600

    Update shelters_controller.rb

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,6 +1,6 @@
 class Admin::SheltersController < ApplicationController
   def index
-   @shelters = Shelter.raw_sql_query
+   @shelters = Shelter.shelters_by_name
    @shelters_with_apps = Shelter.shetler_w_apps
   end
 end
