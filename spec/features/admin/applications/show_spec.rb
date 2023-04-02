718baa95488ac6c29e7ec3ffbcb2bf8e7458ffbb
Katie Lonsdale <katie952@gmail.com>
2023-04-02 15:33:06 -0600

    test: that reject button and functionality works

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -50,4 +50,40 @@ RSpec.describe 'the admin application show page' do
       end
     end
   end
+
+  describe "For every pet that the application is for, I see a button to reject the application for that specific pet"
+    it 'for each pet there is a button to reject' do 
+      visit "/admin/applications/#{@application_2.id}"
+
+      within("#application-info") do
+        expect(page).to have_selector(:link_or_button, 'Reject Spike')
+      end
+    end
+  
+  describe "When I click that button then I'm taken back to the admin application show page" do
+    it 'can click button and is redirected' do
+      visit "/admin/applications/#{@application_2.id}"
+      
+      click_on 'Reject Spike'
+    
+      expect(current_path).to eq("/admin/applications/#{@application_2.id}")
+    end
+  end
+
+  describe 'Next to the pet that I rejected, I do not see a button to reject this pet' do
+    it 'hides reject button after a pet application has been rejected' do
+      visit "/admin/applications/#{@application_2.id}"
+
+      click_on 'Reject Spike'
+
+      within("#application-info") do
+        expect(page).to_not have_selector(:link_or_button, 'Reject Spike')
+        expect(page).to have_content("Pet Status: Rejected")
+      end
+
+      within("#application-status") do
+        expect(page).to have_content("Rejected")
+      end
+    end
+  end
 end
