579873bb21d8e83d633863bee699c69850cb81bc
Katie Lonsdale <katie952@gmail.com>
2023-04-04 18:57:46 -0600

    refactor: remove uneccessary strong params

--- app/models/pets_application.rb
+++ app/models/pets_application.rb
@@ -1,9 +1,4 @@
 class PetsApplication < ApplicationRecord
   belongs_to :application
   belongs_to :pet
-
-  private
-  def pets_application_params
-    params.permit(:pet_id, :application_id)
-  end
 end
