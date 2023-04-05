4dc7aa15119552f0d3efc2d4e508bc3f5a20e828
Katie Lonsdale <katie952@gmail.com>
2023-04-04 18:33:58 -0600

    feat: add strong params

--- app/models/pets_application.rb
+++ app/models/pets_application.rb
@@ -1,4 +1,9 @@
 class PetsApplication < ApplicationRecord
   belongs_to :application
   belongs_to :pet
+
+  private
+  def pets_application_params
+    params.permit(:pet_id, :application_id)
+  end
 end
