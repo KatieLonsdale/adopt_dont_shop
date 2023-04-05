d231670fbb82486b5bce0d55d284144c36ef69af
Katie Lonsdale <katie952@gmail.com>
2023-04-04 18:34:40 -0600

    refactor: add more validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,11 +2,11 @@ class Application < ApplicationRecord
   has_many :pets_applications
   has_many :pets, through: :pets_applications
 
-  validates :applicant, presence: true
+  validates :applicant, presence: true, length: { minimum: 2 }
   validates :street_address, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :zip_code, presence: true
+  validates :zip_code, presence: true, length: { is: 5}, numericality: { only_integer: true }
 
   def find_app_pets
     pets = PetsApplication.select(:pet_id).where(application_id: id)
